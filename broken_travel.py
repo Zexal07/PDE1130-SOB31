8ec5ad7cd4e6546cfdd124030d4bc9b8d27e742e
Zexal07 <aa4897@live.mdx.ac.uk>
2021-11-15 14:33:59 +0400

    Update broken_travel.py
    fixed first set of the if/else conditional statements by adding a colon and fixing incorrect syntax with apostrophe

--- broken_travel.py
+++ broken_travel.py
@@ -8,8 +8,8 @@
 
 year == int(input("Greetings! What is your year of origin? "))
 
-if year <= 1900
-    print ('Woah, that's the past!')
+if year <= 1900:
+    print ("Woah, that's the past!")
 elif year > 1900 && year < 2020:
     print ("That's totally the present!")
 elif:
